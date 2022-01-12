43f1215d8296d44bb21524bc0ecf9efa9aaf76d4
marie-debug <marionakinyi.ma@gmail.com>
2022-01-12 16:55:26 +1000

    added requirements txt and Procfile

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ def index():
     socialmedia = data["socialmedia"]
 
     return render_template('index.html', data=data, projectslen=len(projects), socialmedialen=len(socialmedia))
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
